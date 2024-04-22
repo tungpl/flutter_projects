af30c88eaf5445408f3d63282864c34bb9ed680a
tungpl <>
2024-04-21 21:52:35 -0500

    small update

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -19,7 +19,7 @@ class GradientContainer extends StatelessWidget {
       ),
       child: const Center(
           child: Text(
-        'Hello Booiii!!!',
+        'Hello Boo!!!',
         style: TextStyle(
           color: Colors.white,
           fontSize: 28,
