317688392e969ae6243055d9f682e005e746a60f
tungpl <>
2024-04-21 22:00:01 -0500

    refactor style_text

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/style_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
@@ -18,13 +19,8 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: const Center(
-          child: Text(
-        'Hello Boo!!!',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 28,
-        ),
-      )),
+        child: StyleText(),
+      ),
     );
   }
 }
---
+++ first_app/lib/style_text.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class StyleText extends StatelessWidget {
+  const StyleText({super.key});
+
+  @override
+  Widget build(Object context) {
+    return const Text(
+      'Hello Boofeee!!!',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 28,
+      ),
+    );
+  }
+}
