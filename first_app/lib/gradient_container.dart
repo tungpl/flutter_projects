bc437e68ae1c48aaf5978f976277582445d3d220
tungpl <>
2024-04-21 22:30:53 -0500

    valuables

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/style_text.dart';
 
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
@@ -14,8 +17,8 @@ class GradientContainer extends StatelessWidget {
             Color.fromARGB(255, 26, 105, 122),
             Color.fromARGB(255, 195, 136, 54),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
