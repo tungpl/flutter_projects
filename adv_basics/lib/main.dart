f47e388fdde1e685b721abfb5f37a18bb618f546
tungpl <>
2024-05-01 12:08:30 -0500

    adv-basics

---
+++ adv_basics/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: StartScreen(),
+      ),
+    ),
+  );
+}
