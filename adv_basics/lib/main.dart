ad4aa9c7118e5e120c868e0d090f8451c24ba2c4
tungpl <>
2024-05-01 12:52:23 -0500

    add Text and button

--- adv_basics/lib/main.dart
+++ adv_basics/lib/main.dart
@@ -3,9 +3,17 @@ import 'package:adv_basics/start_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 101, 42, 205),
+              Color.fromARGB(255, 105, 67, 170)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const StartScreen(),
+        ),
       ),
     ),
   );
